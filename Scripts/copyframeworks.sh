9c8e811b46591e53f109ff39ac675d58911a9961
Scott Gruby <scott@gruby.com>
2015-01-01 15:56:16 -0800

    - Updated copy frameworks scripts

--- Scripts/copyframeworks.sh
+++ Scripts/copyframeworks.sh
@@ -4,4 +4,5 @@ cd ${TARGET_BUILD_DIR}
 FRAMEWORKS_PATH="${TARGET_BUILD_DIR}/${EXECUTABLE_FOLDER_PATH}/Frameworks"
 mkdir -p "${FRAMEWORKS_PATH}"
 cp -r "${TARGET_BUILD_DIR}/Alamofire.framework" "${FRAMEWORKS_PATH}"
+cp -r "${TARGET_BUILD_DIR}/CryptoSwift.framework" "${FRAMEWORKS_PATH}"
 cp -r "${TARGET_BUILD_DIR}/Vera.framework" "${FRAMEWORKS_PATH}"
