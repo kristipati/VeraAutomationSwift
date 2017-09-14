6832c07051f578bc2919152f598b91a333008f59
Scott Gruby <scott@gruby.com>
2017-09-13 19:48:54 -0700

    % Updated script (again)

--- Scripts/export.sh
+++ Scripts/export.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -x
 export
 chmod +x ${WORKSPACE}/Scripts/archive.swift
-${WORKSPACE}/Scripts/archive.swift ${EXPORT_METHOD} "${TEAM_ID}" "${WORKSPACE}/Build/" "${WORKSPACE}" "${PROVISIONING_PROFILE_NAME}"
-mv ${WORKSPACE}/Build/*.ipa "${WORKSPACE}/${JOB_NAME}_${SHORT_VERSION}_${BUILD_ID}.ipa"
+${WORKSPACE}/Scripts/archive.swift ${EXPORT_METHOD} "${TEAM_ID}" "${WORKSPACE}/Build/" "${WORKSPACE}/Build" "${PROVISIONING_PROFILE_NAME}"
+cp ${WORKSPACE}/Build/*.ipa "${WORKSPACE}/${JOB_NAME}_${SHORT_VERSION}_${BUILD_ID}.ipa"
 cd ${WORKSPACE}/Build/Release-iphoneos
 zip -r ../../${JOB_NAME}_${SHORT_VERSION}_${BUILD_ID}-dSYM.zip *.dSYM
